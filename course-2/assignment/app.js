6e8cbf7ca4f7ba8c49ace6374742c13e5e79692e
Jigme Lodey <90403145+jigmeloday@users.noreply.github.com>
2023-08-01 22:00:56 +0600

    course 3

--- course-2/assignment/app.js
+++ course-2/assignment/app.js
@@ -3,7 +3,7 @@ const app = Vue.createApp({
         return{
             name: 'Jigme Lodey',
             inputValue: 'Test',
-            age: '121',
+            age: 121,
             imageLink: 'https://images.unsplash.com/photo-1532012197267-da84d127e765?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=774&q=80'
         }
     },

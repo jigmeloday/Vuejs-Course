f2beb2826a63cea585e560338db83b24bae197d8
Jigme Lodey <90403145+jigmeloday@users.noreply.github.com>
2023-08-01 22:52:10 +0600

    events with arg

--- course-3/app.js
+++ course-3/app.js
@@ -2,10 +2,14 @@ const app = Vue.createApp({
     data() {
         return{
             counter: 0,
-            value: 0
+            value: 0,
+            name: ''
         }
     },
     methods: {
+        setName(value) {
+            this.name = value.target.value
+        },
         add(number) {
             this.counter += number
         },

e8ca8aaf7b90abec931d41e32dd945e66bbcbf54
Jigme Lodey <90403145+jigmeloday@users.noreply.github.com>
2023-08-01 21:14:12 +0600

    dynamic goal dispay

--- assignment-2/app.js
+++ assignment-2/app.js
@@ -1,15 +1,16 @@
 const app = Vue.createApp({
     data() {
         return{
-            courseGoal: 'Hello Vue',
+            courseGoalA: 'Hello Vue',
+            courseGoalB: 'Hello Vue',
             vueLink: 'https://vuejs.org'
         }
     },
     methods: {
         randomCourse() {
             const random = Math.random();
-            if(random % 2 === 0) return 'Happy Vue'
-            return 'Happy React'
+            if(random % 2 === 0) return this.courseGoalA;
+            return this.courseGoalB;
         }
     }
 })

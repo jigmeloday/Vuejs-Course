b6cc7466ae0bd7987e061ba4c3577eb280dd9a1f
Jigme Lodey <90403145+jigmeloday@users.noreply.github.com>
2023-08-02 14:31:43 +0600

    game updates

--- moster-game/app.js
+++ moster-game/app.js
@@ -6,14 +6,39 @@ const app = Vue.createApp({
         return {
             mosterHealth: 100,
             playerHealth: 100,
-            currentRound: 0
+            currentRound: 0,
+            winner: null
         }
+    },
+    watch: {
+       playerHealth(value) {
+        if(!value && !this.mosterHealth) {
+            this.winner = 'draw'
+        }
+        else if (!value) {
+            this.winner = 'moster'
+        }
+       },
+       mosterHealth(value) {
+        if(!value && !this.playerHealth) {
+            this.winner = 'draw'
+        }
+        else if (!value) {
+            this.winner = 'player'
+        }
+       }
     },
     computed: {
         mosterHealthBar() {
+            if(this.mosterHealth <= 0) {
+                return '0%'
+            }
             return this.mosterHealth + '%';
         },
         playerHealthBar() {
+            if(this.playerHealth <= 0) {
+                return '0%'
+            }
             return this.playerHealth + '%';
         },
         eligibleForSpecailAttack() {
@@ -27,22 +52,43 @@ const app = Vue.createApp({
     methods: {
         attackMoster(type) {
             this.currentRound ++;
-            const attackValue = randomValue(5, 12);
-            this.mosterHealth -= attackValue;
+            this.mosterHealth -= randomValue(5, 12);
+
             this.attackPlayer();
+            
         },
         specialAttack() {
             this.currentRound ++;
-            this.mosterHealth -= randomValue(12, 20);
+            const specail = randomValue(12, 20);
+            this.mosterHealth -= specail;
+            if(this.mosterHealth - specail >=0 ){
+               
+               } else{
+                this.mosterHealth =0;
+             }
         },
         attackPlayer() {
-            this.playerHealth -= randomValue(8, 15);
+            const health = randomValue(8, 15);
+
+            if(this.playerHealth - health >= 0 ){
+                this.playerHealth -= health;
+               } else{
+                this.playerHealth =0;
+               }
         },
         surrender() {
-            this.playerHealth -= 100;
+            this.playerHealth = 0;
         },
         healPlayer() {
-            this.playerHealth += randomValue(2,10);
+            this.currentRound ++;
+            this.attackPlayer();
+            const healedValue = randomValue(8,20);
+            if(healedValue+this.playerHealth <=100) {
+                this.playerHealth += healedValue;
+            } else {
+                this.playerHealth = 100;
+            }
+            
         }
     }
 })

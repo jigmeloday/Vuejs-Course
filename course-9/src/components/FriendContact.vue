6febc3b254db6022c032f33f26b40fc8abbcd19b
Jigme Lodey <90403145+jigmeloday@users.noreply.github.com>
2023-08-02 23:03:53 +0600

    vue project

---
+++ course-9/src/components/FriendContact.vue
@@ -0,0 +1,41 @@
+<template>
+  <li>
+    <h1>{{friend.name}}</h1>
+    <button @click='toggleShow'>{{ `${!showDetail? 'Show' : 'Hide'} Details` }}</button>
+    <ul v-if="showDetail">
+        <li>
+            <strong>
+                Phone:
+            </strong>
+            {{friend.phone}}
+        </li>
+        <li>
+            <strong>
+                Phone:
+            </strong>
+            {{friend.email}}
+        </li>
+    </ul>
+  </li>
+</template>
+
+<script>
+    export default {
+        data() {
+            return{
+                showDetail: false,
+                friend:  {
+                    id: '1',
+                    name: 'pema',
+                    phone: '12121212',
+                    email: 'pema@gmail.com'
+                },
+            }
+        },
+        methods: {
+            toggleShow() {
+                this.showDetail = !this.showDetail
+            }
+        }
+    };
+</script>

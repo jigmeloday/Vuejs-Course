649f1bf623b9965b4fb3cea9d0cbcb3411d248a7
Jigme Lodey <90403145+jigmeloday@users.noreply.github.com>
2023-08-02 12:27:15 +0600

    game set up

--- moster-game/app.js
+++ moster-game/app.js
@@ -1,7 +1,34 @@
+function randomValue(min, max) {
+    return Math.floor(Math.random() * (max-min)) + min;
+}
 const app = Vue.createApp({
     data() {
         return {
+            mosterHealth: 100,
+            playerHealth: 100
 
+        }
+    },
+    computed: {
+        mosterHealthBar() {
+            return this.mosterHealth + '%';
+        },
+        playerHealthBar() {
+            return this.playerHealth + '%';
+        }
+    },
+    methods: {
+        attackMoster(type) {
+            const attackValue = randomValue(5, 12)
+            this.mosterHealth -= attackValue;
+            this.attackPlayer();
+        },
+        attackPlayer() {
+            this.playerHealth -= randomValue(8, 15)
+        },
+
+        healPlayer() {
+            this.playerHealth += randomValue(2,10)
         }
     }
 })

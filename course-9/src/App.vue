c0b503c9e9a3ee22b5cfc57ed1e5f0bbcf8e8718
Jigme Lodey <90403145+jigmeloday@users.noreply.github.com>
2023-08-02 20:26:09 +0600

    vue set up

---
+++ course-9/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+    <section>
+        <h2>My Friends</h2>
+        <ul>
+            <li v-for="friend in friends" :key='friend.id'>{{friend}}</li>
+        </ul>
+    </section>
+</template>
+
+<script>
+export default{
+
+    data() {
+        return{
+            friends: [
+                {
+                    id: '1',
+                    name: 'pema',
+                    phone: '12121212',
+                    email: 'pema@gmail.com'
+                },
+                {
+                    id: '2',
+                    name: 'sema',
+                    phone: '12121212',
+                    email: 'sema@gmail.com'
+                },
+                {
+                    id: '3',
+                    name: 'dema',
+                    phone: '12121212',
+                    email: 'dema@gmail.com'
+                }
+            ]
+        }
+    }
+}
+</script>

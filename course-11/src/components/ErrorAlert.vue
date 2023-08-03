87040f5b2b5ae82f9cd952b0c176721e010eb51b
Jigme Lodey <90403145+jigmeloday@users.noreply.github.com>
2023-08-03 22:13:11 +0600

    course 11

---
+++ course-11/src/components/ErrorAlert.vue
@@ -0,0 +1,18 @@
+<template>
+  <dialog open>
+    <slot></slot>
+  </dialog>
+</template>
+
+<style scoped>
+dialog {
+  margin: 0;
+  position: absolute;
+  top: 20vh;
+  left: 30%;
+  width: 40%;
+  background: white;
+  padding: 1rem;
+  box-shadow: 0 2px 8px rbga(0, 0, 0, 0.26);
+}
+</style>
--- course-11/src/components/ManageGoal.vue
+++ course-11/src/components/ManageGoal.vue
@@ -1,18 +1,37 @@
 <template>
   <div>
     <h2>Manage Goals</h2>
-    <input type="text" refs="goal" />
+    <input type="text" ref="goal" />
     <button @click="setGoals">Set Goal</button>
+    <Teleport to="body">
+      <error-alert v-if="inputInvalid">
+        <h2>Empty Input</h2>
+        <p>Please Enter at least a few character</p>
+        <button @click="confirmError">Ok</button>
+      </error-alert>
+    </Teleport>
   </div>
 </template>
 <script>
+import ErrorAlert from './ErrorAlert.vue';
 export default {
+  components: {
+    ErrorAlert,
+  },
+  data() {
+    return {
+      inputInvalid: false,
+    };
+  },
   methods: {
     setGoals() {
       const enterValue = this.$refs.goal.value;
       if (enterValue === '') {
-        alert('Empty Input');
+        this.inputInvalid = true;
       }
+    },
+    confirmError() {
+      this.inputInvalid = false;
     },
   },
 };

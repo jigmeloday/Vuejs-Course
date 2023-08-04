f5536d5dde490c8143963d8be9ec9c304c0f75ec
Jigme Lodey <90403145+jigmeloday@users.noreply.github.com>
2023-08-04 19:29:24 +0600

    course 14

--- course-14/src/components/survey/LearningSurvey.vue
+++ course-14/src/components/survey/LearningSurvey.vue
@@ -9,7 +9,13 @@
         </div>
         <h3>My learning experience was ...</h3>
         <div class="form-control">
-          <input type="radio" id="rating-poor" value="poor" name="rating" v-model="chosenRating" />
+          <input
+            type="radio"
+            id="rating-poor"
+            value="poor"
+            name="rating"
+            v-model="chosenRating"
+          />
           <label for="rating-poor">Poor</label>
         </div>
         <div class="form-control">
@@ -23,12 +29,18 @@
           <label for="rating-average">Average</label>
         </div>
         <div class="form-control">
-          <input type="radio" id="rating-great" value="great" name="rating" v-model="chosenRating" />
+          <input
+            type="radio"
+            id="rating-great"
+            value="great"
+            name="rating"
+            v-model="chosenRating"
+          />
           <label for="rating-great">Great</label>
         </div>
-        <p
-          v-if="invalidInput"
-        >One or more input fields are invalid. Please check your provided data.</p>
+        <p v-if="invalidInput">
+          One or more input fields are invalid. Please check your provided data.
+        </p>
         <div>
           <base-button>Submit</base-button>
         </div>
@@ -46,7 +58,7 @@ export default {
       invalidInput: false,
     };
   },
-  emits: ['survey-submit'],
+  // emits: ['survey-submit'],
   methods: {
     submitSurvey() {
       if (this.enteredName === '' || !this.chosenRating) {
@@ -55,11 +67,23 @@ export default {
       }
       this.invalidInput = false;
 
-      this.$emit('survey-submit', {
-        userName: this.enteredName,
-        rating: this.chosenRating,
-      });
-
+      // this.$emit('survey-submit', {
+      //   userName: this.enteredName,
+      //   rating: this.chosenRating,
+      // });
+      fetch(
+        'https://learning-vue-47350-default-rtdb.firebaseio.com/serveys.json',
+        {
+          method: 'POST',
+          header: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            userName: this.enteredName,
+            rating: this.chosenRating,
+          }),
+        }
+      );
       this.enteredName = '';
       this.chosenRating = null;
     },
--- course-14/src/components/survey/UserExperiences.vue
+++ course-14/src/components/survey/UserExperiences.vue
@@ -3,7 +3,9 @@
     <base-card>
       <h2>Submitted Experiences</h2>
       <div>
-        <base-button>Load Submitted Experiences</base-button>
+        <base-button @click="loadServey"
+          >Load Submitted Experiences</base-button
+        >
       </div>
       <ul>
         <survey-result
@@ -21,9 +23,43 @@
 import SurveyResult from './SurveyResult.vue';
 
 export default {
-  props: ['results'],
+  // props: ['results'],
+  data() {
+    return {
+      results: [],
+    };
+  },
   components: {
     SurveyResult,
+  },
+  methods: {
+    loadServey() {
+      fetch(
+        'https://learning-vue-47350-default-rtdb.firebaseio.com/serveys.json',
+        {
+          method: 'GET',
+          headers: {
+            'Content-type': 'application/json',
+          },
+        }
+      )
+        .then((res) => {
+          if (res.ok) {
+            return res.json();
+          }
+        })
+        .then((data) => {
+          const results = [];
+          for (const id in data) {
+            results.push({
+              id: id,
+              name: data[id].userName,
+              rating: data[id].rating,
+            });
+          }
+          this.results = results;
+        });
+    },
   },
 };
 </script>
